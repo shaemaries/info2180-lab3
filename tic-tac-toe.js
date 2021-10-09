6ceb3114bc5ea8440c7922f6ee5c30135ca7e0c8
Shae-Marie Small <shaemarie.small@gmail.com>
2021-10-09 10:24:47 -0400

    Added intital starter files

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -17,8 +17,8 @@ function boardSetup(board) {
         board.getElementsByTagName('div')[i].addEventListener('click',squareClick);
         board.getElementsByTagName('div')[i].addEventListener('mouseover',squareMouseOver);
         board.getElementsByTagName('div')[i].addEventListener('mouseout',squareMouseOut);
-
-    }
+        board.getElementsByTagName('div')[i].setAttribute('id',i);
+     }
 }
 
 function squareClick(clickEvent){
@@ -27,14 +27,19 @@ function squareClick(clickEvent){
     clickedSquare.classList.add(activeplayer);
     clickedSquare.textContent=activeplayer;
 
-    if (activeplayer=="X"){
-        tracker.push(activeplayer);
-        activeplayer="O"
-    }
-    else {
-        tracker.push(activeplayer);
-        activeplayer="X";   
-    }
+
+      if (!checkWin()){  
+          if (activeplayer=="X"){
+            tracker.push(activeplayer);
+            activeplayer="O";
+            clickedSquare.classList.remove(activeplayer);
+        }
+         else {
+            tracker.push(activeplayer);
+             activeplayer="X";  
+             clickedSquare.classList.remove(activeplayer); 
+            }  
+      }
 }
 
 function squareMouseOver(overEvent){
@@ -45,4 +50,82 @@ function squareMouseOver(overEvent){
 function squareMouseOut(outEvent){
     var outSquare=outEvent.target;
     outSquare.classList.remove('hover');
+}
+
+function checkWin(){
+  //horizontal   
+  for (var i=0; i < 3; i++){
+      var startpos= i*3;
+      var endpos=startpos+3;
+      var presentX=0;
+      var presentO=0;  
+
+      for (startpos; startpos<endpos; startpos++){
+          var checkElement = document.getElementById(startpos).textContent;
+          if(checkElement=="X"){
+              presentX+=1;
+            }
+         if(checkElement=="O"){
+            presentO+=1;
+           }   
+        }
+        
+        if (presentX==3 || presentO==3){
+            winMessage();
+            return true
+        }
+    } 
+   
+    //end of horizontal 
+
+    //vertical
+    for (var i=0; i < 3 ; i++){
+        var startpos=i;
+        var presentX=0;
+        var presentO=0;
+    
+
+        for (var r=0; r <3; r++){
+            var checkElement= document.getElementById(startpos).textContent;
+            if(checkElement=="X"){
+                 presentX+=1;
+            }                  
+            else if(checkElement=="O"){
+                presentO+=1;
+            } 
+            startpos+=3; 
+        } 
+        if (presentX==3 || presentO==3){
+            winMessage();
+            return true
+        } 
+                 
+        
+    }
+
+   //end of vertical 
+      
+    //diagional
+    if((document.getElementById(0).textContent=="X") && (document.getElementById(4).textContent=="X") && (document.getElementById(8).textContent=="X") || (document.getElementById(0).textContent=="O") && (document.getElementById(4).textContent=="O") && (document.getElementById(8).textContent=="O")) {
+        winMessage();
+        return true
+    }
+
+    if((document.getElementById(2).textContent=="X") && (document.getElementById(4).textContent=="X") && (document.getElementById(6).textContent=="X")||(document.getElementById(2).textContent=="O") && (document.getElementById(4).textContent=="O") && (document.getElementById(6).textContent=="O")){
+        winMessage();
+        return true;
+    }// end of digional code 
+
+
+  if (tracker.length==8){
+        var tieMessage= document.getElementById('status');
+        tieMessage.textContent="Game ended in a tie. Try again";
+    }
+}
+
+
+function winMessage(){
+    var winMessage= document.getElementById('status');
+        winMessage.classList.add('you-won');
+        winMessage.textContent="Congratulations! "+ activeplayer+ " is the Winner";
 }
